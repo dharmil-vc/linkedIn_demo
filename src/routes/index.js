d30c0cb375c1d1fe62a9c29a0dfc4aed25e2e259
dharmil-vc <dharmil.shah@viitor.cloud>
2022-11-11 16:26:37 +0530

    Setup swagger-ui-express

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+
 import { getAccesstoken } from '../controller/auth.controller.js';
+// import { retrievememberProfile } from '../controller/signin.controller.js';
 
 const router = express.Router();
+const options = {
+  customCss: '.swagger-ui .topbar { display: none }',
+};
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/swagger.yml'));
+
+router.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options)
+);
 
 // Exchange Authorization Code for an Access Token Route
 router.post('/authToken', getAccesstoken);
